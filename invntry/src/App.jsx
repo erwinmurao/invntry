36a22933bb367f10e73a4557284f3d53eab68064
Jyc <erwinmurao1123@gmail.com>
2026-04-25 13:32:25 +0800

    invntry file

---
+++ invntry/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import LandingPage from  '../src/component/features/home'
+const App = () => {
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  )
+}
+
+export default App
---
+++ invntry/src/component/features/home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const home = () => {
+  return (
+    <div>
+   sample
+    </div>
+  );
+};
+
+export default home;
